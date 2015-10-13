ffd83bd9f8dc173d5b0bdf6514a30408ed9e4b53
Amber Crawford <ammcrawford@gmail.com>
2015-10-13 09:16:08 -0600

    Added styling

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -3,13 +3,17 @@ require_relative 'task'
 
 class TaskManager
   def self.database
+    # holds info that was submitted
     @database ||= YAML::Store.new("db/task_manager")
   end
 
   def self.create(task)
+    # opens file
     database.transaction do
+      # find tasks section, make sure it's there or give empty array
       database['tasks'] ||= []
       database['total'] ||= 0
+      # counter to setup id #
       database['total'] += 1
       database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
     end
